b3aabaa44f0baec6ca1f7e736399b0566c0a23e9
matanovm <matanov.martin@gmail.com>
2018-09-21 11:09:06 +0300

    Stack and Queues Exercise

---
+++ Stack And Queues/08.StackFibbonaci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int input = int.Parse(Console.ReadLine());
+            var stack = new Stack<long>(input);
+
+            stack.Push(1);
+            stack.Push(1);
+
+            for (int i = 0; i < input - 1; i++)
+
+            {
+
+                var first = stack.Pop();
+                var second = stack.Pop();
+
+
+                stack.Push(first);
+                stack.Push(first + second);
+
+            }
+
+            stack.Pop();
+
+            Console.WriteLine(stack.Peek());
+
+
+
+        }
+
+    }
+
