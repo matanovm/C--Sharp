ff6ef9594e79514c5d1c88e9c589d0a8c3a73698
matanovm <matanov.martin@gmail.com>
2018-10-02 15:28:38 +0300

    Modify Ex.:7 and Add Ex.:8

--- Multidimentional Arrays/07. Lego Blocks/Program.cs
+++ Multidimentional Arrays/07. Lego Blocks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _07._Lego_Blocks
 {
@@ -6,7 +7,53 @@ namespace _07._Lego_Blocks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rowsNum = int.Parse(Console.ReadLine());
+
+            int[][] leftBlock = new int[rowsNum][];
+            int[][] rightBlock = new int[rowsNum][];
+
+            for (int r = 0; r < rowsNum; r++)
+            {
+                leftBlock[r] = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+            }
+
+            for (int r = 0; r < rowsNum; r++)
+            {
+                rightBlock[r] = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).Reverse().ToArray();
+            }
+
+            int rowLenght = leftBlock[0].Length + rightBlock[0].Length;
+            int allElements = 0;
+            bool isMatch = true;
+
+            for (int r = 0; r < rowsNum; r++)
+            {
+                int currentLenght = leftBlock[r].Length + rightBlock[r].Length;
+                if (currentLenght != rowLenght)
+                {
+                    isMatch = false;
+                }
+
+                allElements += currentLenght;
+            }
+
+            if (isMatch)
+            {
+                int[] resultRow = new int[rowLenght];
+                for (int r = 0; r < rowsNum; r++)
+                {
+                    resultRow = leftBlock[r].Concat(rightBlock[r]).ToArray();
+                    Console.WriteLine($"[{string.Join(", ", resultRow)}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The total number of cells is: {allElements}");
+            }
         }
     }
 }
---
+++ Multidimentional Arrays/11. Parking System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11._Parking_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
