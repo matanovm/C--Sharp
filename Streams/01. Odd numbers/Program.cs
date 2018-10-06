0a19a1cb271867b72222f21a3ddd58ebb96777ab
matanovm <matanov.martin@gmail.com>
2018-10-06 15:52:10 +0300

    Add Ex.: 1,2

---
+++ Streams/01. Odd numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _01._Odd_Lines
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = @"D:\GitHub_Repository\C--Sharp\Streams\Streams\01. Odd numbers\text.txt";
+            using (var stream = new StreamReader(path))
+            {
+                var lineNumber = 0;
+
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    if (lineNumber % 2 ==1)
+                    {
+                        Console.WriteLine(line);
+                        
+                    }
+                    lineNumber++;
+                }
+
+            }
+        }
+    }
+}
