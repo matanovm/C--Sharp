be029ea3406548f908616ba09cb6b5068809a4e5
matanovm <matanov.martin@gmail.com>
2018-10-06 14:58:20 +0300

    Streams Exercises

---
+++ Streams/Streams/06. Zipping Sliced Files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Zipping_Sliced_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
