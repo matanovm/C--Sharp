e0fdcdf4e8c5a70c81732c55a7b99a8e8afa6360
matanovm <matanov.martin@gmail.com>
2018-10-13 17:36:48 +0300

    update

--- Streams/04. Copy Binary File/Program.cs
+++ Streams/04. Copy Binary File/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
 
-namespace _04._Copy_Binary_File
+namespace _04.Copy_Binary_File
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string CopyFile = @"D:\GitHub_Repository\C--Sharp\Streams\04. Copy Binary File\image.jpg";
+            using (var sourceFile = new FileStream(CopyFile , FileMode.Open))
+            {
+                string NewFile = @"D:\GitHub_Repository\C--Sharp\Streams\04. Copy Binary File\copiedimage.jpg";
+                using (var destinacionFile = new FileStream(NewFile , FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        var readBytesCount = sourceFile.Read(buffer, 0, buffer.Length);
+                        if (readBytesCount == 0)
+                            break;
+
+                        destinacionFile.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
         }
     }
 }
