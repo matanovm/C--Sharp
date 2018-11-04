61bc4d6a4ead24c842cc18ed16757bb0a242b82b
matanovm <matanov.martin@gmail.com>
2018-11-05 01:17:20 +0200

    Add Ex.:2,3

--- Exam 03.09.2017/02. Crypto Master/Program.cs
+++ Exam 03.09.2017/02. Crypto Master/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.Crypto_Master
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine()
+				.Split(',')
+				.Select(int.Parse)
+				.ToArray();
+
+			int maxSequence = 0;
+
+			for (int index = 0; index < numbers.Length; index++)
+			{
+				for (int step = 1; step < numbers.Length; step++)
+				{
+					int currentIndex = index;
+					int nextIndex = (index + step) % numbers.Length;
+					int currentSequence = 1;
+
+					while (numbers[currentIndex] < numbers[nextIndex])
+					{
+						currentIndex = nextIndex;
+						nextIndex = (nextIndex + step) % numbers.Length;
+						currentSequence++;
+					}
+
+					if (currentSequence > maxSequence)
+					{
+						maxSequence = currentSequence;
+					}
+				}
+			}
+
+			Console.WriteLine(maxSequence);
+		}
+	}
 }
--- Exam 03.09.2017/04. Treasure Map/Program.cs
+++ Exam 03.09.2017/04. Treasure Map/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace _04.Treasure_Map
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string pattern = @"((?<hash>#)|!)[^#!]*?(?<![A-Za-z0-9])(?<streetName>[A-Za-z]{4})(?![A-Za-z0-9])[^#!]*(?<!\d)(?<streetNumber>\d{3})-(?<password>\d{4}|\d{6})(?!\d)[^#!]*?(?(hash)!|#)";
+			int count = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < count; i++)
+			{
+				string input = Console.ReadLine();
+				MatchCollection matches = Regex.Matches(input, pattern);
+				Match correctMatch = matches[matches.Count / 2];
+				string streetName = correctMatch.Groups["streetName"].Value;
+				string streetNumber = correctMatch.Groups["streetNumber"].Value;
+				string password = correctMatch.Groups["password"].Value;
+				Console.WriteLine($"Go to str. {streetName} {streetNumber}. Secret pass: {password}.");
+			}
+		}
+	}
 }
