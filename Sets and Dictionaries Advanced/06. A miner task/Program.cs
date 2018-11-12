e4973f2813ef690b2d6190df0887be083bd356d8
matanovm <matanov.martin@gmail.com>
2018-11-12 03:12:13 +0200

    Add ex 4,6,7,8,9

---
+++ Sets and Dictionaries Advanced/06. A miner task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.A_miner_task
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, int> mats = new Dictionary<string, int>();
+			string material = Console.ReadLine();
+
+			while (material != "stop")
+			{
+				int quantity = 0;
+
+				quantity = int.Parse(Console.ReadLine());
+
+				if (mats.ContainsKey(material))
+				{
+					mats[material] += quantity;
+				}
+				else
+				{
+					mats.Add(material, quantity);
+				}
+				material = Console.ReadLine();
+			}
+			PrintSortedResults(mats);
+		}
+
+		private static void PrintSortedResults(Dictionary<string, int> mats)
+		{
+			foreach (KeyValuePair<string, int> materials in mats)
+			{
+				Console.WriteLine($"{materials.Key} -> {materials.Value}");
+			}
+		}
+	}
+}
+
---
+++ Sets and Dictionaries Advanced/07. Fix emails/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.Fix_emails
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = Console.ReadLine();
+			Dictionary<string, string> emails = new Dictionary<string, string>();
+			while (name != "stop")
+			{
+				string email = Console.ReadLine();
+				string ending = email.Substring(email.Length - 2, 2).ToLower();
+				if (ending != "uk" && ending != "us")
+				{
+					emails.Add(name, email);
+				}
+				name = Console.ReadLine();
+			}
+			foreach (var pair in emails)
+			{
+				Console.WriteLine("{0} -> {1}", pair.Key, pair.Value);
+			}
+		}
+	}
+}
+
